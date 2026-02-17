54a28cfb1987c03c16b8556abb1179ced6cd2155
Samarth-Prajapati <samarthp475@gmail.com>
2026-02-17 17:49:06 +0530

    Update : Added models needed in api.

--- FinanceTrackerAPI/apis/models.py
+++ FinanceTrackerAPI/apis/models.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, StrictStr, StrictFloat, Field, field_validator, StrictInt
-from typing import List
+from pydantic import BaseModel, StrictStr, StrictFloat, Field, field_validator
+from typing import List, Optional
 import datetime
 from enum import Enum
 
@@ -13,15 +13,15 @@ class TypeCategory(Enum):
     both = "both"
 
 class TransactionModel(BaseModel):
-    title : StrictStr = Field(min_length = 3, max_length = 100)
-    description : StrictStr = Field(max_length = 500)
-    amount : StrictFloat = Field(gt = 0)
+    title : StrictStr = Field(..., min_length = 3, max_length = 100)
+    description : StrictStr = Field(..., max_length = 500)
+    amount : StrictFloat = Field(..., gt = 0)
     type : TypeTransaction
     category : StrictStr
     date : datetime.date
-    tags : List[StrictStr] = List[Field(max_length = 10)]
+    tags : List[str] = Field(..., max_length = 10)
     created_at : datetime.datetime
-    updated_at : datetime.datetime
+    updated_at : None
 
     class Config:
         orm_mode = True
@@ -49,11 +49,55 @@ class TransactionModel(BaseModel):
 class CategoryModel(BaseModel):
     name : StrictStr
     type : TypeCategory
-    description : StrictStr = Field(max_length = 500)
+    description : StrictStr = Field(..., max_length = 500)
     created_at: datetime.datetime
 
     class Config:
         orm_mode = True
         use_enum_values = True
 
+class DeleteBulk(BaseModel):
+    category : str
+
+    class Config:
+        orm_mode = True
+
+    @field_validator("category")
+    @classmethod
+    def validate_category(cls, value : str):
+        if not value.strip():
+            raise ValueError("Category cannot be null.")
+        return value.lower().strip()
+
+class TransactionModelPatch(BaseModel):
+    title : Optional[str] = Field(None, min_length = 3, max_length = 100)
+    description : Optional[str] = Field(None, max_length = 500)
+    amount : Optional[float] = Field(None, gt = 0)
+    type : Optional[TypeTransaction] = None
+    category : Optional[str] = None
+    tags : Optional[List[str]] = Field(None, max_length = 10)
+    updated_at : datetime.datetime
+
+    class Config:
+        orm_mode = True
+        use_enum_values = True
+
+    @field_validator("title")
+    @classmethod
+    def strip_title(cls, value):
+        return value.strip()
+
+    @field_validator("category")
+    @classmethod
+    def validate_category(cls, value):
+        if not value:
+            raise ValueError("Category cannot be null.")
+        return value.lower()
+
+    @field_validator("tags")
+    @classmethod
+    def validate_tags(cls, value):
+        for tag in value:
+            if len(tag) > 30:
+                raise ValueError("Tags cannot be more than 30 characters.")
 
